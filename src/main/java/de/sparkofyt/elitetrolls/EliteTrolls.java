9c6891ce5450bdbd1157ec2bab55579a2b73ef06
sparkofyt <sparkofyt@gmail.com>
2022-03-20 23:04:50 +0100

    Update EliteTrolls.java

--- src/main/java/de/sparkofyt/elitetrolls/EliteTrolls.java
+++ src/main/java/de/sparkofyt/elitetrolls/EliteTrolls.java
@@ -1,17 +1,33 @@
 package de.sparkofyt.elitetrolls;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class EliteTrolls extends JavaPlugin {
 
+    /* Variables*/
+    public static EliteTrolls instance;
+
+    /* Methods */
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        instance = this;
+        sendStartupMessage();
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    private void sendStartupMessage() {
+        Bukkit.getConsoleSender().sendMessage("");
+        Bukkit.getConsoleSender().sendMessage("§a  ___________.__  .__  __       ___________             .__  .__");
+        Bukkit.getConsoleSender().sendMessage("§a  \\_   _____/|  | |__|/  |_  ___\\__    ___/______  ____ |  | |  |   ______");
+        Bukkit.getConsoleSender().sendMessage("§a   |    __)_ |  | |  \\   __\\/ __ \\|    |  \\_  __ \\/  _ \\|  | |  |  /  ___/");
+        Bukkit.getConsoleSender().sendMessage("§a   |        \\|  |_|  ||  | \\  ___/|    |   |  | \\(  <_> )  |_|  |__\\___ \\ ");
+        Bukkit.getConsoleSender().sendMessage("§a  /_______  /|____/__||__|  \\___  >____|   |__|   \\____/|____/____/____  >");
+        Bukkit.getConsoleSender().sendMessage("§a          \\/                    \\/                                     \\/       v1.0 by sparkofyt");
+        Bukkit.getConsoleSender().sendMessage("");
+    }
+
+    /* Getters & Setters */
+    public static EliteTrolls getInstance() {
+        return instance;
     }
 }
