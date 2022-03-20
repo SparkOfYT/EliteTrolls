eef24473746a2198ad1a20dce8991a7b2f3d40ad
sparkofyt <sparkofyt@gmail.com>
2022-03-20 22:18:51 +0100

    Initial commit

---
+++ src/main/java/de/sparkofyt/elitetrolls/EliteTrolls.java
@@ -0,0 +1,17 @@
+package de.sparkofyt.elitetrolls;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EliteTrolls extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
